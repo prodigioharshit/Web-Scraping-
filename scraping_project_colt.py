75bdf6c8bc490d3e22af7a10dfed347dd500ff3c
prodigioharshit <harshit19051915@gmail.com>
2020-07-22 00:21:23 +0530

    play again logic

--- scraping_project_colt.py
+++ scraping_project_colt.py
@@ -23,13 +23,38 @@ while url:
     url = next_btn.find("a")["href"] if next_btn else None
     # sleep(2)
 
-quote = choice(all_quotes)
-remaining_guesses = 4
-print("Here's a quote... ")
-print(quote["text"])
-print(quote['author'])
-guess = ''    
-while guess.lower() != quote["author"].lower():
-    guess = input(f"Who said this? Guesses remaing: {remaining_guesses}")
+def start_game():
+    quote = choice(all_quotes)
+    remaining_guesses = 4
+    print("Here's a quote... ")
+    print(quote["text"])
+    print(quote['author'])
+    guess = ''    
+    while guess.lower() != quote["author"].lower():
+        guess = input(f"Who said this? Guesses remaing: {remaining_guesses} ")
+        if guess == quote['author'].lower():
+            print("YOU GOT IT RIGHT")
+            break
+        remaining_guesses -= 1
+        if remaining_guesses == 3:
+            res = requests.get(f"{base_url}{quote['bio-link']}")
+            soup = BeautifulSoup(res.text,"html.parser")
+            birthdate = soup.find(class_="author-born-date").get_text()
+            birthday = soup.find(class_="author-born-location").get_text()
+            print(f"Here's a hint: The author was born on {birthdate} {birthday}")
+        elif remaining_guesses == 2:
+            print(f"Here's a hint: The author's first name starts with {quote['author'][0]}")
+        elif remaining_guesses == 1:
+            last_initial = quote['author'].split(" ")[1][0]
+            print(f"Here's a hint: The author's last name starts with {last_initial}")
+        else:
+            print(f"Sorry you ran out of guesses. The answer is {quote['author']}")
+            break
 
-print("AFTER WHILE LOOP")
+    again = ''
+    while not again.lower() in ('yes','y','n','no'):
+        again = input("Would you like to play again? (y/n)")
+    if again.lower() in ('yes','y'):
+        return start_game()
+    else:
+        print("Ok!GoodBye...")
