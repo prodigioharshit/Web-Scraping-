f070a2c9b587a2932d59497c0f9d6c0316b0266c
prodigioharshit <harshit19051915@gmail.com>
2020-07-21 23:41:25 +0530

    Add quotes,author and link to desc

--- scraping_project_colt.py
+++ scraping_project_colt.py
@@ -1,6 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+all_quotes = []
 response = requests.get("http://quotes.toscrape.com")
 soup = BeautifulSoup(response.text,"html.parser")
-print(soup.body)
+quotes = soup.find_all(class_="quote")
+for quote in quotes:
+    all_quotes.append({
+    "text" : quote.find(class_="text").get_text(),
+    "author" : quote.find(class_="author").get_text(),
+    "bio-link" : quote.find("a")["href"]
+    }) 
+
+print(all_quotes)
+    
